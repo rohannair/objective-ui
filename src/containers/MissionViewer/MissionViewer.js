a330eabdd3ee7e69f6491bf231edf0cb887773f8
Ray KananI <hello@raykanani.com>
2016-09-29 13:54:36 -0400

    Update mission view card style

--- src/containers/MissionViewer/MissionViewer.js
+++ src/containers/MissionViewer/MissionViewer.js
@@ -15,46 +15,52 @@ const MissionView = props =>
       </div>
       <div className={ styles.profileDetails }>
         <h4>Ray Kanani</h4>
-        <h5>{ props.mission.get('name')}</h5>
+        <h5>{ props.mission.get('name')} - {props.mission.get('duration')}</h5>
       </div>
     </div>
+    <div className={ styles.divider }></div>
     <Card>
-      <h4>Objective</h4>
-      <p>{props.mission.get('description')}</p>
+      <div className={ styles.section }>
+        <h4>Objective</h4>
+        <p>{props.mission.get('description')}</p>
+      </div>
 
-      <h4>Duration</h4>
-      <p>{props.mission.get('duration')}</p>
+      <div className={ styles.section }>
+        <h4>Objectives and Key Results</h4>
+        <ul>
+          { props.mission.get('okrs').map(o =>
+            <li key={o.get('id')}>
+              {o.get('objective')}
+              <ul>
+                {
+                  o.get('keyResults').map((r,i) =>
+                    <li key={`${o.get('id')}-${i}`}>{r}</li>)
+                }
+              </ul>
+            </li>)
+          }
+        </ul>
+      </div>
 
-      <h4>Objectives and Key Results</h4>
-      <ul>
-        { props.mission.get('okrs').map(o =>
-          <li key={o.get('id')}>
-            {o.get('objective')}
-            <ul>
-              {
-                o.get('keyResults').map((r,i) =>
-                  <li key={`${o.get('id')}-${i}`}>{r}</li>)
-              }
-            </ul>
-          </li>)
-        }
-      </ul>
+      <div className={ styles.section }>
+        <h4>Personal Objectives</h4>
+        <ul>
+          {
+            props.mission.get('objectives').map(o =>
+              <li key={o.get('id')}>{o.get('name')}</li>)
+          }
+        </ul>
+      </div>
 
-      <h4>Personal Objectives</h4>
-      <ul>
-        {
-          props.mission.get('objectives').map(o =>
-            <li key={o.get('id')}>{o.get('name')}</li>)
-        }
-      </ul>
-
-      <h4>Resources</h4>
-      <ul>
-        {
-          props.mission.get('resources').map(o =>
-            <li key={o.get('id')}>{o.get('name')}</li>)
-        }
-      </ul>
+      <div className={ styles.section }>
+        <h4>Resources</h4>
+        <ul>
+          {
+            props.mission.get('resources').map(o =>
+              <li key={o.get('id')}>{o.get('name')}</li>)
+          }
+        </ul>
+      </div>
     </Card>
   </div>
 
