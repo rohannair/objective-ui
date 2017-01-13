7e058a3dae42c16635c5f463a3f83f7aee791be7
Rohan Nair <rohan@objectiveiq.com>
2017-01-13 14:34:36 -0500

    Adding in Likes to UI (#58)
    * Adding in deleteReaction mutation and styling
    
    * Making tests pass...

--- src/components/Snapshot/Snapshot.js
+++ src/components/Snapshot/Snapshot.js
@@ -7,6 +7,7 @@ import dateformat from 'dateformat'
 
 import Pill from '../Pill'
 import UserTab from '../UserTab'
+import Reaction from '../Reaction'
 
 /**
  * While this should theoretically be a stateless component, it is
@@ -14,12 +15,22 @@ import UserTab from '../UserTab'
  * the lifecycle methods
  */
 class Snapshot extends Component {
+  static defaultProps = {
+    viewer: { id: null },
+    deleteReaction: () => {},
+    addReaction: () => {},
+  }
+
   render() {
-    const { snap, showObjective } = this.props
+    const { viewer: { id: viewerId }, snap, showObjective, addReaction, deleteReaction } = this.props
+    const isLiked = snap.reactions && snap.reactions.reduce((val, reaction) => reaction.user.id === viewerId, false)
+
     return (
       <div className={styles.snapshot}>
         <header className={styles.snapshot__head} >
           <UserTab {...snap.user}>
+            { showObjective && snap.objective && <Pill info>{snap.objective.name}</Pill>}
+            { snap.blocker && <Pill danger><i className={'zmdi zmdi-alert-triangle'} /> BLOCKER!</Pill>}
             <small>{dateformat(snap.createdAt, 'mmm dd h:MM TT')}</small>
           </UserTab>
         </header>
@@ -30,12 +41,43 @@ class Snapshot extends Component {
         />
 
         <footer className={styles.snapshot__footer}>
-          { snap.blocker && <Pill danger><i className={'zmdi zmdi-alert-triangle'} /> BLOCKER!</Pill>}
-          { showObjective && snap.objective && <Pill info>{snap.objective.name}</Pill>}
+          <Reaction
+            isLiked={isLiked}
+            onClick={
+              isLiked
+              ? deleteReaction.bind(this, 1, snap.id)
+              : addReaction.bind(this, 1, snap.id)
+            }
+           />
+           <span style={{
+             fontSize: '0.85em',
+             color: '#f36',
+             fontWeight: 'bold'
+           }}>{ snap.reactions && snap.reactions.length}</span>
         </footer>
       </div>
     )
   }
+}
+
+Snapshot.mutations = {
+  addReaction: gql`
+    mutation addReaction($reactionId: Int!, $snapshotId: Int!) {
+      addReaction(reactionId: $reactionId, snapshotId: $snapshotId) {
+        id
+        ...ReactionBoxFragment
+      }
+    }
+    ${Reaction.fragments.reaction}
+  `,
+
+  deleteReaction: gql`
+    mutation deleteReaction($reactionId: Int!, $snapshotId: Int!) {
+      deleteReaction(reactionId: $reactionId, snapshotId: $snapshotId) {
+        id
+      }
+    }
+  `
 }
 
 Snapshot.fragments = {
@@ -51,8 +93,14 @@ Snapshot.fragments = {
         id
         ...UserTabFragment
       }
+      reactions {
+        id
+        ...ReactionBoxFragment
+      }
+
     }
     ${UserTab.fragments.user}
+    ${Reaction.fragments.reaction}
   `
 }
 
