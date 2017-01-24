1c857b590350d86aba11b3683bd8c807e71d7076
Rohan Nair <r@rohannair.ca>
2017-01-24 12:19:18 -0500

    Fixing datepicker tests

--- src/components/Datepicker/Datepicker.js
+++ src/components/Datepicker/Datepicker.js
@@ -3,16 +3,18 @@ import DatePicker from 'react-toolbox/lib/date_picker'
 import React, { PropTypes } from 'react'
 import theme from './Datepicker.css'
 
-const datetime = new Date(2015, 10, 16)
-const min_datetime = new Date(new Date(datetime).setDate(8))
-
-const ThemedDatepicker = p => (
+const ThemedDatepicker = ({
+  label,
+  minDate = new Date('2017-01-01'),
+  onChange,
+  defaultValue
+}) => (
   <DatePicker
     theme={theme}
-    label={ p.label }
-    minDate={ min_datetime }
-    onChange={ p.onChange }
-    defaultValue={ p.defaultValue }
+    label={ label }
+    minDate={ minDate || min_datetime }
+    onChange={ onChange }
+    defaultValue={ defaultValue }
   />
 )
 
--- src/components/Datepicker/datepicker.test.js
+++ src/components/Datepicker/datepicker.test.js
@@ -4,7 +4,7 @@ import Datepicker from './index.js'
 
 describe('Test suite for Datepicker component', () => {
   it('Datepicker should exist', () => {
-    let wrapper = shallow(<Datepicker/>)
+    let wrapper = shallow(<Datepicker />)
     expect(wrapper).toMatchSnapshot()
   })
 })
