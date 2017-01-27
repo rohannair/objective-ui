ed030d4c168780939dfde23187b6761de7500794
Ghislain <telitos@gmail.com>
2017-01-27 12:57:01 -0500

    Refactors dates.js

--- src/utils/dates.js
+++ src/utils/dates.js
@@ -1,30 +1,12 @@
 /*This modules formats dates as required.*/
 import dateformat from 'dateformat'
 
-export const toTimestamp = (dt) => {
-  const date = +new Date(dt)
-  return date
-}
+export const timestamp = (dt = null) => !dt ? +new Date() : +new Date(dt)
 
-export const newDate = (dt) => {
-  let date = new Date(dt)
-  return date
-}
-
-export const timestampNow = () => {
-  let date = Date.now()
-  return date
-}
+export const newDate = (dt = null) => !dt ? new Date() : new Date(dt)
 
 export const toLocalMonthDayYear = (milliseconds) => {
-  const intDate = parseInt(milliseconds)
-  const dt = new Date(intDate)
+  const dt = new Date(milliseconds)
   const date = dateformat(dt, 'mmmm dd, yyyy')
-  return date
-}
-
-export const timestampToDate = (milliseconds) => {
-  const intDate = parseInt(milliseconds)
-  const date = new Date(intDate)
   return date
 }
