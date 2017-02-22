f20f775af4548550ddeaea19b0cde69f53dd3dcb
David Mills <dave@1morepx.com>
2017-02-22 14:50:51 -0500

    🔨 Refactor snapshot component

--- src/components/ObjectiveFeed/ObjectiveFeed.js
+++ src/components/ObjectiveFeed/ObjectiveFeed.js
@@ -4,31 +4,15 @@ import { Link } from 'react-router'
 
 import styles from './ObjectiveFeed.css'
 
-import SnapshotContainer from '../SnapshotContainer'
-import SnapshotHeader from '../SnapshotHeader'
-import SnapshotBody from '../SnapshotBody'
-import SnapshotFooter from '../SnapshotFooter'
 import SnapshotEditor from '../SnapshotEditor'
+import Snapshot from '../Snapshot'
 
 const ObjectiveFeed = p => {
   const snapshots = (p.snapshots.length < 1)
   ? <EmptyObjectiveFeed />
   : p.snapshots.map(snap => {
-    const isLiked = snap.reactions.some(r => r && r.user.id === p.viewer.id)
     return (
-      <SnapshotContainer key={snap.id}>
-        <SnapshotHeader {...snap} editObjective={p.editSnapshotObjective.bind(this, snap)}/>
-        <SnapshotBody
-          img={snap.img}
-          body= {snap.body }
-          bodyJson={snap.bodyJson}
-        />
-        <SnapshotFooter
-          count={snap.reactions.length}
-          isLiked={isLiked}
-          readOnly
-        />
-      </SnapshotContainer>
+      <Snapshot key={snap.id} snap={snap} editSnapshotObjective={p.editSnapshotObjective} viewer={p.viewer} />
     )
   })
 
@@ -47,39 +31,18 @@ const EmptyObjectiveFeed = () => (
   <Link to="/feed">Add some snapshots from the Feed</Link>
 )
 
-const NEW_SNAPSHOT = gql`
-  mutation addSnapshot($bodyJson: String, $objective: String, $blocker: Boolean, $img: String) {
-    addSnapshot(bodyJson: $bodyJson, objective: $objective, blocker: $blocker, img: $img) {
-      id
-      body
-      bodyJson
-      img
-      ...SnapshotHeaderFragment
-      ...SnapshotFooterFragment
-    }
-  }
-  ${SnapshotHeader.fragments.header}
-  ${SnapshotFooter.fragments.footer}
-`
-
 ObjectiveFeed.mutations = {
-  NEW_SNAPSHOT
+  NEW_SNAPSHOT: Snapshot.mutations.NEW_SNAPSHOT
 }
 
 ObjectiveFeed.fragments = {
   feed: gql`
     fragment ObjectiveFeedFragment on Objective {
       snapshots {
-        id
-        body
-        bodyJson
-        img
-        ...SnapshotHeaderFragment
-        ...SnapshotFooterFragment
+        ...SnapshotFeedFragment
       }
     }
-    ${SnapshotHeader.fragments.header}
-    ${SnapshotFooter.fragments.footer}
+    ${Snapshot.fragments.feed}
   `
 }
 
